c37c08abcc601bf32e2553215d36829b0040958d
Conner Soule <csconner1998@gmail.com>
2020-05-13 20:00:15 -0600

    Create LOR TEST.py
    Added my stuff which prints out top 400 something players in Masters then every 30 seconds monitors if any changes happen to ladder

---
+++ LOR TEST.py
@@ -0,0 +1,28 @@
+import requests
+import time
+API_KEY = 'insert API key here'
+params = dict(api_key=API_KEY)
+url = "https://americas.api.riotgames.com/lor/ranked/v1/leaderboards"
+playerdict = {}
+response = requests.get(url, params=params)
+print(response.status_code)
+json = response.json()
+for x in json['players']:
+    playerdict[str(x['name'])] = str(x['rank'])
+    string = str(x['name']) + ' is rank: ' + str(x['rank'])
+    print(string)
+while True:
+    print("Doing stuff...")
+    # do your stuff
+    response = requests.get(url, params=params)
+    json = response.json()
+    for x in json['players']:
+        if str(x['name']) not in playerdict and playerdict[str(x['name'])] != int(x['rank']):
+            string = " "
+            if str(x['name']) not in playerdict:
+                string = 'NEW PLAYER: ' + str(x['name']) + ' is rank: ' + str(x['rank'])
+            else:
+                string = (str(x['name']) + ' WENT FROM: ' + playerdict[str(x['name'])] + ' TO: ' + str(x['rank']))
+            playerdict[str(x['name'])] = int(x['rank'])
+            print(string)
+    time.sleep(30)
